3ea317959bf8ec8ef1f6e4fe3c155e2bb3e62c08
RICHARD GRIFFITHS <griff182uk@yahoo.co.uk>
2021-12-12 13:51:20 +0000

    got doc generation working

--- validate_schema_example.py
+++ validate_schema_example.py
@@ -2,14 +2,27 @@ import json
 import jsonschema
 from jsonschema import validate
 
-
 def get_json_file(file_name):
     """This function loads the given json available"""
     with open(file_name, 'r') as file:
         schema = json.load(file)
     return schema
 
-json_schema = get_json_file('teams.schema.json')
-json_data = get_json_file('teams.json')
+def validate_schema(json_data, json_schema):
 
-validate(instance=json_data, schema=json_schema)
+    try:
+        validate(instance=json_data, schema=json_schema)
+        print('Schema validation passed.')
+    except jsonschema.exceptions.ValidationError as err:
+        print(f"Unexpected {err}, {type(err)}")
+        raise
+
+data_file = 'teams.json'
+schema_file = 'teams.schema.json'
+json_schema = get_json_file(schema_file)
+json_data = get_json_file(data_file)
+validate_schema(json_data=json_data,json_schema=json_schema)
+
+print(json.dumps(json_data,indent=4))
+
+data = json.load(json_data)
