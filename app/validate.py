84fcdc241229af799bd7560930c6cde26bc743b0
RICHARD GRIFFITHS <griff182uk@yahoo.co.uk>
2022-01-03 11:09:23 +0000

    updated schema validation

--- app/validate.py
+++ app/validate.py
@@ -2,19 +2,29 @@ import json
 import jsonschema
 from jsonschema import validate
 import os
+from os import listdir
+from os.path import isfile, join
 import sys
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from lib.teamconfig import *
 
 # Must be schema valid
-
-data_file = 'teams/platform_team.json'
 schema_file = 'teams/schema/team.schema.json'
 json_schema = get_json_file(schema_file)
-json_data = get_json_file(data_file)
-validate_schema(json_data=json_data,json_schema=json_schema)
+mypath = 'teams'
+teams = []
 
+team_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+for file in team_files:
+  data_file = f'{mypath}/{file}'
+  json_data = get_json_file(data_file)
+  print(f"Validating {file}...")
+  validate_schema(json_data=json_data,json_schema=json_schema)
+  print(f"Validated {file}.")
 # Must have a PO, Scrum Master and Technical Lead
 
 # Team members must only be in one team
+
+
+print("Schema validation passed")
